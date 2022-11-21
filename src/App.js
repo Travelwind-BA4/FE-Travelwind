a524705212d5a72dea7fa99f931a56429d5114e8
nizarfazari <nexsel32@gmail.com>
2022-11-21 22:50:30 +0700

    setup dan instalasi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import NavBar from "./compenent/NavBar/NavBar";
 import RoutesApp from "./routes";
 
 function App() {
   return (
     <div>
-      <NavBar/>
       <RoutesApp />
     </div>
   );
