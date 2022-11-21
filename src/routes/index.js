1c14bfec08e13d8b8b1ffecbba5d64ebaa611b58
gihonhon <agunggihon@gmail.com>
2022-11-22 01:17:37 +0800

    add section change flight

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "../pages/Homepage";
+import Resultpage from "../pages/Resultpage";
 
 const index = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/result" element={<Resultpage />} />
       </Routes>
     </div>
   );
