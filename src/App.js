a194517693d65e18848c3a4530e9fb4d523a2e14
gihonhon <agunggihon@gmail.com>
2022-11-21 20:28:57 +0800

    first navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import NavBar from "./compenent/NavBar/NavBar";
 import RoutesApp from "./routes";
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <RoutesApp />
     </div>
   );
