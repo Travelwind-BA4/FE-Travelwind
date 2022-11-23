9e55b2c3e91ae55c3e5304e72a0e6038122bb4b2
gihonhon <agunggihon@gmail.com>
2022-11-23 19:38:02 +0800

    fix component fix styling fix layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "../pages/Homepage";
-import Resultpage from "../pages/Resultpage";
+import Resultpage from "../pages/resultPage/Resultpage";
 
 const index = () => {
   return (
