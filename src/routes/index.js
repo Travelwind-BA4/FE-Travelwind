cd1ecc4b2b3f4a9b2cfb1b12e869e1f4b7ae58bc
gihonhon <agunggihon@gmail.com>
2022-11-25 15:32:56 +0800

    add loginPage add registerPage
    responsive on progress function on progress

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import Homepage from "../pages/Homepage";
+import Loginpage from "../pages/loginPage/Loginpage";
+import Registerpage from "../pages/registerPage/Registerpage";
 import Resultpage from "../pages/resultPage/Resultpage";
 
 const index = () => {
@@ -12,6 +14,8 @@ const index = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/result" element={<Resultpage />} />
+        <Route path="/login" element={<Loginpage/>} />
+        <Route path="/register" element={<Registerpage/>}/>
       </Routes>
       <Footer />
     </div>
