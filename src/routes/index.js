18f5c3aa2bdee78ffbda47c053a3ab1c429150c8
nizarfazari <nexsel32@gmail.com>
2022-12-01 12:54:31 +0700

    paymentpage serta mobile responsive

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ import Loginpage from "../pages/loginPage/Loginpage";
 import Registerpage from "../pages/registerPage/Registerpage";
 import Resultpage from "../pages/Resultpage";
 import Profilpage from "../pages/Profilpage";
+import Paymentpage from "../pages/Paymentpage";
 
 const Index = () => {
   const { pathname } = useLocation();
@@ -29,6 +30,7 @@ const Index = () => {
       <Routes>
         <Route path="/results" element={<Resultpage />} />
         <Route path="/flight" element={<Detailpage />} />
+        <Route path="/payment" element={<Paymentpage />} />
         <Route path="/account/profile" element={<Profilpage />} />
         <Route path="/account/orders" element={<Profilpage />} />
       </Routes>
