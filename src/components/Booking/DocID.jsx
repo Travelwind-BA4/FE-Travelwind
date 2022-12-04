8ed48e53db2798c9a9b3c04ceb8f28c116662904
Kris Adiwinata <99080684+FeaRisk@users.noreply.github.com>
2022-12-05 05:48:50 +0700

    Menambahkan component tipe dokumen pd traveler information

---
+++ src/components/Booking/DocID.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Select } from "antd";
+const onChange = (value) => {
+  console.log(`selected ${value}`);
+};
+const onSearch = (value) => {
+  console.log("search:", value);
+};
+
+const items = [
+  {
+    value: "id",
+    label: "ID Card",
+  },
+
+  {
+    value: "pass",
+    label: "Passport",
+  },
+];
+const DocID = ({ placeholder, item }) => {
+  return (
+    <div>
+      <Select
+        showSearch
+        defaultValue={"id"}
+        className="w-[100%] border-b-2 border-gray-300"
+        bordered={false}
+        placeholder={placeholder}
+        optionFilterProp="children"
+        onChange={onChange}
+        onSearch={onSearch}
+        filterOption={(input, option) =>
+          (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+        }
+        options={items}
+      />
+    </div>
+  );
+};
+
+export default DocID;
