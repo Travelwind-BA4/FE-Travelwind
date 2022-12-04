f79390c60408660e24a9b7d1ba112c67c5f2ba38
Kris Adiwinata <99080684+FeaRisk@users.noreply.github.com>
2022-12-05 05:19:32 +0700

    Menambahkan traveler information pada Booking Page

--- src/pages/Bookingpage.jsx
+++ src/pages/Bookingpage.jsx
@@ -2,16 +2,20 @@ import { useNavigate } from "react-router-dom";
 import Select1 from "../components/Booking/Contact";
 import Title1 from "../components/Booking/Title1";
 import Country from "../components/Booking/Ctry";
+import Birth from "../components/Booking/DateBirth";
 
 const BookingPage = () => {
   const navigate = useNavigate();
   return (
-    <div className="container mx-auto py-10 px-10 h-screen">
+    <div className="container mx-auto py-10 px-10">
       <div className="grid lg:grid-cols-3 grid-cols-2 gap-y-8">
+        {/* Contact Information */}
         <div className="col-span-2 bg-[#f1f5f5]  px-8 pb-8  rounded-md">
           <div>
-            <form class="mt-5 w-full max-w-lg">
+            <div className="border-b border-gray-300 mt-5">
               <p className="font-semibold text-xl mb-5">Contact Information</p>
+            </div>
+            <form class="mt-5 w-full max-w-lg">
               <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full px-3">
                   <label
@@ -32,7 +36,7 @@ const BookingPage = () => {
                     Title <span className="text-red-500">*</span>
                   </label>
                   <div class="relative">
-                    <Title1 placeholder={"Mr"}></Title1>
+                    <Title1 placeholder={"Mr/Mrs"}></Title1>
                   </div>
                 </div>
               </div>
@@ -48,7 +52,7 @@ const BookingPage = () => {
                     class="appearance-none block w-full bg-[#f1f5f5] text-sm border-b-2 border-gray-300 text-gray-700 py-1.5 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     id="grid-city"
                     type="text"
-                    placeholder="0"
+                    placeholder="John"
                   />
                 </div>
                 <div class="w-full md:w-1/2 px-3">
@@ -62,7 +66,7 @@ const BookingPage = () => {
                     class="appearance-none block w-full bg-[#f1f5f5] text-sm border-b-2 border-gray-300 text-gray-700 py-1.5 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     id="grid-city"
                     type="text"
-                    placeholder="0"
+                    placeholder="Smith"
                   />
                 </div>
               </div>
@@ -127,8 +131,111 @@ const BookingPage = () => {
             </button>
           </div>
         </div>
+
+        {/* Traveler Information */}
+        <div className="col-span-2 bg-[#f1f5f5]  px-8 pb-8  rounded-md">
+          <div className="border-b border-gray-300 mt-5">
+            <p className="font-semibold text-xl mb-5">Traveler Information</p>
+          </div>
+          <div className="border shadow-md py-5 px-3">
+            <p className="font-semibold text-xl mb-3">
+              Traveler 1 <span>|</span> Dewasa
+            </p>
+            <div className=" border-b border-gray-300"></div>
+            <p className="mt-2 text-sm px-3 text-orange-400 font-semibold">
+              Nama sesuai KTP/Passpor tanpa gelar dan tanda baca
+            </p>
+            <form class="mt-3 w-full max-w-lg px-3">
+              <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                  <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="grid-state"
+                  >
+                    Title <span className="text-red-500">*</span>
+                  </label>
+                  <div class="relative">
+                    <Title1 placeholder={"Mr/Mrs"}></Title1>
+                  </div>
+                </div>
+              </div>
+              <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                  <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="grid-first-name"
+                  >
+                    First Name <span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    class="appearance-none block w-full bg-[#f1f5f5] text-sm border-b-2 border-gray-300 text-gray-700 py-1.5 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-city"
+                    type="text"
+                    placeholder="John"
+                  />
+                </div>
+                <div class="w-full md:w-1/2 px-3">
+                  <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="grid-last-name"
+                  >
+                    Last Name <span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    class="appearance-none block w-full bg-[#f1f5f5] text-sm border-b-2 border-gray-300 text-gray-700 py-1.5 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-city"
+                    type="text"
+                    placeholder="Smith"
+                  />
+                </div>
+              </div>
+              <div class="flex flex-wrap -mx-3 mb-2">
+                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                  <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="grid-state"
+                  >
+                    Date Birth <span className="text-red-500">*</span>
+                  </label>
+                  <div class="relative">
+                    <Birth></Birth>
+                  </div>
+                </div>
+                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                  <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="grid-city"
+                  >
+                    Phone Number <span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    class="appearance-none block w-full bg-[#f1f5f5] text-sm border-b-2 border-gray-300 text-gray-700 py-1.5 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-city"
+                    type="text"
+                    placeholder="0"
+                  />
+                </div>
+                <div class="w-full px-3 mt-6">
+                  <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="grid-password"
+                  >
+                    Email <span className="text-red-500">*</span>
+                  </label>
+                  <input
+                    class="appearance-none block w-full bg-[#f1f5f5] text-sm border-b-2 border-gray-300 text-gray-700 py-1.5 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-city"
+                    type="text"
+                    placeholder="Ex.Binar@Airsans.com"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
+
 export default BookingPage;
