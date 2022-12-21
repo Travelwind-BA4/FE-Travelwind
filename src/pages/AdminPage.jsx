2c8dd829d1f21b28ed4ca571aa35322501e4fe7a
Kris Adiwinata <99080684+FeaRisk@users.noreply.github.com>
2022-12-21 17:25:40 +0700

    Menambahkan page Admin

---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,11 @@
+import SideBar from "../components/Sidebar/Sidebar";
+
+const AdminPage = () => {
+  return (
+    <div>
+      <SideBar></SideBar>
+    </div>
+  );
+};
+
+export default AdminPage;
