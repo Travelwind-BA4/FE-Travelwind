7eeec46a88944635c6b77024043cfd79deca7d29
Kris Adiwinata <99080684+FeaRisk@users.noreply.github.com>
2022-12-22 13:06:30 +0700

    menambahkan navigasi sidebar

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import {
   DesktopOutlined,
-  FileOutlined,
   PieChartOutlined,
-  TeamOutlined,
   UserOutlined,
+  LogoutOutlined,
 } from "@ant-design/icons";
 import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { useNavigate } from "react-router-dom";
+
 const { Header, Content, Footer, Sider } = Layout;
 function getItem(label, key, icon, children) {
   return {
@@ -16,20 +17,24 @@ function getItem(label, key, icon, children) {
     label,
   };
 }
+
 const items = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Users", "2", <DesktopOutlined />),
-  getItem("Data", "sub1", <UserOutlined />, [
-    getItem("Country", "3"),
-    getItem("City", "4"),
-    getItem("Airport", "5"),
-    getItem("Airplane", "6"),
-    getItem("Terminal", "7"),
-    getItem("Gate", "8"),
-    getItem("Schedule", "9"),
+  getItem("Dashboard", "/admin", <PieChartOutlined />),
+  getItem("Users", "/admin/user", <UserOutlined />),
+  getItem("Data", "sub1", <DesktopOutlined />, [
+    getItem("Country", "data/country"),
+    getItem("City", "data/city"),
+    getItem("Airport", "data/airport"),
+    getItem("Airplane", "data/airplane"),
+    getItem("Terminal", "data/terminal"),
+    getItem("Gate", "data/gate"),
+    getItem("Schedule", "data/schedule"),
   ]),
+  getItem("Sign Out", "/login", <LogoutOutlined />),
 ];
+
 const SideBar = () => {
+  const navigate = useNavigate();
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer },
@@ -56,47 +61,59 @@ const SideBar = () => {
           theme="dark"
           defaultSelectedKeys={["1"]}
           mode="inline"
+          icon=""
+          onClick={({ key }) => {
+            if (key === "signout") {
+              navigate("/login");
+            } else {
+              navigate(key);
+            }
+          }}
           items={items}
         />
       </Sider>
-      <Layout className="site-layout">
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        />
-        <Content
-          style={{
-            margin: "0 16px",
-          }}
-        >
-          <Breadcrumb
+      <>
+        <Layout className="site-layout">
+          <Header
             style={{
-              margin: "16px 0",
-            }}
-          >
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
+              padding: 0,
               background: colorBgContainer,
             }}
           >
-            Bill is a cat.
-          </div>
-        </Content>
-        <Footer
-          style={{
-            textAlign: "center",
-          }}
-        >
-          TravelWind ©2022
-        </Footer>
-      </Layout>
+            Header
+          </Header>
+          <Content
+            style={{
+              margin: "0 16px",
+            }}
+          >
+            <Breadcrumb
+              style={{
+                margin: "16px 0",
+              }}
+            >
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+                background: colorBgContainer,
+              }}
+            >
+              Content
+            </div>
+          </Content>
+          <Footer
+            style={{
+              textAlign: "center",
+            }}
+          >
+            TravelWind ©2022
+          </Footer>
+        </Layout>
+      </>
     </Layout>
   );
 };
