f4313beee7443be791b4f7c16ea2932e90a84924
Kris Adiwinata <99080684+FeaRisk@users.noreply.github.com>
2022-12-22 16:14:35 +0700

    Menambahkan folder component table admin

---
+++ src/components/Admin/Table/showCountry.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Space, Table, Tag } from "antd";
+const columns = [
+  {
+    title: "Country Code",
+    dataIndex: "countryCD",
+    key: "countryCD",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Country Name",
+    dataIndex: "countryName",
+    key: "countryName",
+  },
+  {
+    title: "Telephone Code",
+    dataIndex: "telephoneCD",
+    key: "telephoneCD",
+  },
+  {
+    title: "Action",
+    key: "action",
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Add</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+const data = [
+  {
+    key: "1",
+    countryCD: "21",
+    countryName: "Indonesia",
+    telephoneCD: "+62",
+  },
+  {
+    key: "2",
+    countryCD: "69",
+    countryName: "Japan",
+    telephoneCD: "+81",
+  },
+  {
+    key: "3",
+    countryCD: "11",
+    countryName: "America",
+    telephoneCD: "+1",
+  },
+];
+
+const showCountry = () => {
+  return <Table columns={columns} dataSource={data} />;
+};
+export default showCountry;
