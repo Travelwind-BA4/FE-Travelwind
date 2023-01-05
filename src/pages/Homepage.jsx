5b9fdcb16ec0a301efd475503a2d454d5513daa8
Kris Adiwinata <99080684+FeaRisk@users.noreply.github.com>
2023-01-05 23:48:37 +0700

    adding different time for result page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,20 @@
 import { GiAirplaneArrival, GiAirplaneDeparture } from "react-icons/gi";
 import { AiOutlineUserAdd } from "react-icons/ai";
-import { BsCalendarDate, BsSearch, BsCalendar4Event, BsBookmarkCheck, BsCheckSquare } from "react-icons/bs";
+import {
+  BsCalendarDate,
+  BsSearch,
+  BsCalendar4Event,
+  BsBookmarkCheck,
+  BsCheckSquare,
+} from "react-icons/bs";
 import "../styles/Home.css";
-import { header_1, promotion_1, promotion_2, promotion_3, promotion_4 } from "../assets/images/home";
+import {
+  header_1,
+  promotion_1,
+  promotion_2,
+  promotion_3,
+  promotion_4,
+} from "../assets/images/home";
 import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 
@@ -25,21 +37,38 @@ const Homepage = () => {
   useEffect(() => {
     getAirports();
   }, []);
-  const searchFlight = ({ departureAirport, arrivalAirport, departureDate, traveler }) => {
+  const searchFlight = ({
+    departureAirport,
+    arrivalAirport,
+    departureDate,
+    traveler,
+  }) => {
     localStorage.setItem("traveler", JSON.stringify(traveler));
-    navigate(`results/search?depDate=${timeConverter(departureDate)}&depAirport=${departureAirport}&arrAirport=${arrivalAirport}&traveler=${traveler}`);
+    navigate(
+      `results/search?depDate=${timeConverter(
+        departureDate
+      )}&depAirport=${departureAirport}&arrAirport=${arrivalAirport}&traveler=${traveler}`
+    );
   };
 
   return (
     <>
       <section className="relative sm:mb-[250px] mb-[450px]">
-        <Navbar nav="absolute top-0 w-full z-10 text-white" notif="text-white" />
+        <Navbar
+          nav="absolute top-0 w-full z-10 text-white"
+          notif="text-white"
+        />
         <div className="h-[75vh] header">
-          <div className=" bg-header bg-cover brightness-[0.85] w-full h-full absolute top-0" style={{ backgroundImage: `url(${header_1})`, zIndex: "-2" }}></div>
+          <div
+            className=" bg-header bg-cover brightness-[0.85] w-full h-full absolute top-0"
+            style={{ backgroundImage: `url(${header_1})`, zIndex: "-2" }}
+          ></div>
           <div className="flex flex-col justify-center h-full items-center text-white z-10 text-center">
             <h2 className="font-medium text-4xl mb-4"> Experience Your</h2>
             <h1 className="font-bold text-6xl  mb-6">Best Flight With Us</h1>
-            <p className="text-xl">We have more than 1 million happy customer arround the world</p>
+            <p className="text-xl">
+              We have more than 1 million happy customer arround the world
+            </p>
           </div>
           <div className="flex justify-center">
             <div className="border-2 rounded-xl shadow-md bg-[#fff] absolute sm:bottom-[-140px] bottom-[-400px] px-10 py-3 mx-5 lg:min-w-[800px] ">
@@ -47,15 +76,26 @@ const Homepage = () => {
                 <Trip />
               </div>
               <div className="flex justify-center">
-                <Form className="grid xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5 my-8 px-2" onFinish={searchFlight}>
+                <Form
+                  className="grid xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-5 my-8 px-2"
+                  onFinish={searchFlight}
+                >
                   <div className="flex items-center">
                     <div className="bg-[#f1f5f5] p-3 rounded-xl">
                       <GiAirplaneDeparture className="text-[30px]" />
                     </div>
                     <div className="pl-2">
                       <div>
-                        <h1 className="text-lg font-medium pl-3">Location From</h1>
-                        <Options placeholder="Soekarno Hatta ( JKT ) " name="departureAirport" styles="min-w-[200px]" airports={airports} searchAirport={searchAirport} />
+                        <h1 className="text-lg font-medium pl-3">
+                          Location From
+                        </h1>
+                        <Options
+                          placeholder="Soekarno Hatta ( JKT ) "
+                          name="departureAirport"
+                          styles="min-w-[200px]"
+                          airports={airports}
+                          searchAirport={searchAirport}
+                        />
                       </div>
                     </div>
                   </div>
@@ -65,8 +105,15 @@ const Homepage = () => {
                     </div>
                     <div className="pl-2">
                       <div>
-                        <h1 className="text-lg font-medium pl-3">Location To</h1>
-                        <Options placeholder="Soekarno Hatta ( JKT ) " name="arrivalAirport" airports={airports} styles="min-w-[200px]" />
+                        <h1 className="text-lg font-medium pl-3">
+                          Location To
+                        </h1>
+                        <Options
+                          placeholder="Soekarno Hatta ( JKT ) "
+                          name="arrivalAirport"
+                          airports={airports}
+                          styles="min-w-[200px]"
+                        />
                       </div>
                     </div>
                   </div>
@@ -76,7 +123,9 @@ const Homepage = () => {
                     </div>
                     <div className="pl-2">
                       <div>
-                        <h1 className="text-lg font-medium pl-3">Departure Date</h1>
+                        <h1 className="text-lg font-medium pl-3">
+                          Departure Date
+                        </h1>
                         <Date name="departureDate" style="mb-0" />
                       </div>
                     </div>
@@ -92,13 +141,19 @@ const Homepage = () => {
                       </div>
                     </div>
                     <div className="pl-8 hidden lg:block">
-                      <button className="btn-active p-4 rounded-full" type="submit">
+                      <button
+                        className="btn-active p-4 rounded-full"
+                        type="submit"
+                      >
                         <BsSearch className="text-[20px]" />
                       </button>
                     </div>
                   </div>
                   <div className="col-span-1 sm:col-span-2 lg:hidden">
-                    <button className="btn-active w-full p-4 rounded-full" type="submit">
+                    <button
+                      className="btn-active w-full p-4 rounded-full"
+                      type="submit"
+                    >
                       Search
                     </button>
                   </div>
@@ -112,8 +167,13 @@ const Homepage = () => {
       <section className="py-20 recomendation ">
         <div className="container mx-auto px-10">
           <div className="text-center mb-14">
-            <h1 className="text-5xl font-bold text-[#232730] leading-[60px]">Reccomended For You</h1>
-            <p className="text-[#cfcfcf]">Here you will find all our luxorious tour packages in a reasonable range of cost</p>
+            <h1 className="text-5xl font-bold text-[#232730] leading-[60px]">
+              Reccomended For You
+            </h1>
+            <p className="text-[#cfcfcf]">
+              Here you will find all our luxorious tour packages in a reasonable
+              range of cost
+            </p>
           </div>
           <Homeslider />
         </div>
@@ -125,7 +185,9 @@ const Homepage = () => {
             <h1 className="text-5xl font-bold text-[#232730] leading-[60px]">
               Travel to make memories <br /> all around world
             </h1>
-            <button className="py-4 px-6 bg-[#f1f5f5] border-2 rounded-full mt-5 sm:mt-0">View All</button>
+            <button className="py-4 px-6 bg-[#f1f5f5] border-2 rounded-full mt-5 sm:mt-0">
+              View All
+            </button>
           </div>
           <div className="sm:grid block lg:grid-cols-3 grid-cols-2 gap-x-5 gap-y-5 mt-10 justify-items-center max-w-[1300px] mx-auto">
             <div className="col-span-1 bg-[#fff]  flex justify-center items-center text-center rounded-3xl p-10 shadow-lg max-w-[400px]">
@@ -135,8 +197,13 @@ const Homepage = () => {
                     <BsCalendar4Event className="text-5xl" />
                   </div>
                 </div>
-                <h1 className="font-medium font-sans text-2xl mt-8">BOX & RELAX</h1>
-                <p className=" mt-3">We realize ideas from simple to complex,every become easy to use</p>
+                <h1 className="font-medium font-sans text-2xl mt-8">
+                  BOX & RELAX
+                </h1>
+                <p className=" mt-3">
+                  We realize ideas from simple to complex,every become easy to
+                  use
+                </p>
               </div>
             </div>
             <div className="col-span-1 bg-[#fff]  flex justify-center items-center text-center rounded-3xl p-10 shadow-lg max-w-[400px] my-10 sm:my-0">
@@ -146,8 +213,12 @@ const Homepage = () => {
                     <BsCheckSquare className="text-5xl" />
                   </div>
                 </div>
-                <h1 className="font-medium font-sans text-2xl mt-8">SMART CHECKLISH</h1>
-                <p className=" mt-3">Flight booking in your mind looking for cheap</p>
+                <h1 className="font-medium font-sans text-2xl mt-8">
+                  SMART CHECKLISH
+                </h1>
+                <p className=" mt-3">
+                  Flight booking in your mind looking for cheap
+                </p>
               </div>
             </div>
             <div className="lg:col-span-1 col-span-2 bg-[#fff]  flex justify-center items-center text-center rounded-3xl p-10 shadow-lg max-w-[400px]">
@@ -157,8 +228,13 @@ const Homepage = () => {
                     <BsBookmarkCheck className="text-5xl" />
                   </div>
                 </div>
-                <h1 className="font-medium font-sans text-2xl mt-8">SAVE MORE</h1>
-                <p className=" mt-3">Find cheap flight,travel great list ideals from over travel providers</p>
+                <h1 className="font-medium font-sans text-2xl mt-8">
+                  SAVE MORE
+                </h1>
+                <p className=" mt-3">
+                  Find cheap flight,travel great list ideals from over travel
+                  providers
+                </p>
               </div>
             </div>
           </div>
@@ -166,35 +242,57 @@ const Homepage = () => {
       </section>
       <section className="my-20">
         <div className="container mx-auto px-10">
-          <h1 className="text-center text-4xl font-semibold text-[#232730] mb-12">Why Flight Ticket</h1>
+          <h1 className="text-center text-4xl font-semibold text-[#232730] mb-12">
+            Why Flight Ticket
+          </h1>
           <div className="grid lg:grid-cols-4 grid-cols-2 mt-20 gap-y-20 justify-items-center">
             <div className="col-span-1 text-center max-w-[300px]">
               <div className=" flex justify-center max-h-[140px]">
                 <img src={promotion_1} className="" />
               </div>
-              <h1 className="text-lg font-semibold mt-10 text-[#59595b]">Hassle-Free</h1>
-              <p className="mt-3 text-[#b4b4b7]">Make a transaction from anywhere at any time, from desktop, mobile app, or mobile web. </p>
+              <h1 className="text-lg font-semibold mt-10 text-[#59595b]">
+                Hassle-Free
+              </h1>
+              <p className="mt-3 text-[#b4b4b7]">
+                Make a transaction from anywhere at any time, from desktop,
+                mobile app, or mobile web.{" "}
+              </p>
             </div>
             <div className="col-span-1 text-center max-w-[300px]">
               <div className=" flex justify-center  max-h-[140px]">
                 <img src={promotion_2} className="" />
               </div>
-              <h1 className="text-lg font-semibold mt-10 text-[#59595b]">Service You Can Trust</h1>
-              <p className="mt-3 text-[#b4b4b7]">You get what you paid for – guaranteed.</p>
+              <h1 className="text-lg font-semibold mt-10 text-[#59595b]">
+                Service You Can Trust
+              </h1>
+              <p className="mt-3 text-[#b4b4b7]">
+                You get what you paid for – guaranteed.
+              </p>
             </div>
             <div className="col-span-1 text-center max-w-[300px]">
               <div className=" flex justify-center  max-h-[140px]">
                 <img src={promotion_3} className="" />
               </div>
-              <h1 className="text-lg font-semibold mt-10 text-[#59595b]">Various Payment Options</h1>
-              <p className="mt-3 text-[#b4b4b7]">Be more flexible with various payment methods from ATM, Bank Transfer, Credit Card, and Internet Banking, to Cash.</p>
+              <h1 className="text-lg font-semibold mt-10 text-[#59595b]">
+                Various Payment Options
+              </h1>
+              <p className="mt-3 text-[#b4b4b7]">
+                Be more flexible with various payment methods from ATM, Bank
+                Transfer, Credit Card, and Internet Banking, to Cash.
+              </p>
             </div>
             <div className="col-span-1 text-center max-w-[300px]">
               <div className=" flex justify-center  max-h-[140px]">
                 <img src={promotion_4} className="" />
               </div>
-              <h1 className="text-lg font-semibold mt-10 text-[#59595b]">Secure Transaction Guaranteed</h1>
-              <p className="mt-3 text-[#b4b4b7]">Security and privacy of your online transaction are protected by RapidSSL authorized technology. Receive instant confirmation and e-ticket or voucher directly in your email.</p>
+              <h1 className="text-lg font-semibold mt-10 text-[#59595b]">
+                Secure Transaction Guaranteed
+              </h1>
+              <p className="mt-3 text-[#b4b4b7]">
+                Security and privacy of your online transaction are protected by
+                RapidSSL authorized technology. Receive instant confirmation and
+                e-ticket or voucher directly in your email.
+              </p>
             </div>
           </div>
         </div>
--- src/pages/Resultpage.jsx
+++ src/pages/Resultpage.jsx
@@ -5,6 +5,7 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import { RiSuitcase2Line } from "react-icons/ri";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import useSchedule from "../services/api/useSchedule";
+import convertDiff from "../utils/convertDiff";
 
 import { Select } from "antd";
 
@@ -49,12 +50,16 @@ const Resultpage = () => {
                   <span>
                     {depAirport} - {arrAirport}
                   </span>
-                  <span className="px-1"> | </span> {new Date(depDate).toDateString()}
+                  <span className="px-1"> | </span>{" "}
+                  {new Date(depDate).toDateString()}
                 </p>
               </div>
             </div>
             <div className="flight-header-item sm:block hidden">
-              <button onClick={() => setShowForm(!showForm)} className="text-base text-white rounded-md border bg-transparent border-white border-solid flex w-full py-2 px-6 ">
+              <button
+                onClick={() => setShowForm(!showForm)}
+                className="text-base text-white rounded-md border bg-transparent border-white border-solid flex w-full py-2 px-6 "
+              >
                 Change Search
               </button>
             </div>
@@ -75,13 +80,19 @@ const Resultpage = () => {
           <div className="flex flex-col  bg-[#f1f5f5] rounded-md">
             <div className="flex flex-row justify-between items-center border-b border-gray-300">
               <div className="mx-5 py-2">
-                <p className="font-semibold text-xl mb-1">Departure Flight to Test</p>
+                <p className="font-semibold text-xl mb-1">
+                  Departure Flight to Test
+                </p>
                 <p className="text-base font-light pb-2">
-                  {new Date(depDate).toDateString()} <span> | </span> {`${traveler} Traveler`}
+                  {new Date(depDate).toDateString()} <span> | </span>{" "}
+                  {`${traveler} Traveler`}
                 </p>
               </div>
               <div className="mx-8">
-                <button className="text-base font-medium text-gray-500 rounded-md w-full py-2 px-6 bg-gray-200 hover:bg-gray-100" onClick={() => navigate("/")}>
+                <button
+                  className="text-base font-medium text-gray-500 rounded-md w-full py-2 px-6 bg-gray-200 hover:bg-gray-100"
+                  onClick={() => navigate("/")}
+                >
                   Change Date
                 </button>
               </div>
@@ -91,7 +102,12 @@ const Resultpage = () => {
               <div className="flex flex-row items-center list-filter gap-2 mx-5 py-2">
                 <p>Filter: </p>
                 <div className="flex button-filter gap-3">
-                  <Select className="bg-gray-200 rounded-md" placeholder="Prices" bordered={false} onSelect={lower}>
+                  <Select
+                    className="bg-gray-200 rounded-md"
+                    placeholder="Prices"
+                    bordered={false}
+                    onSelect={lower}
+                  >
                     <Select.Option key={1} value="lower-price">
                       <h1>Lower Prices</h1>
                     </Select.Option>
@@ -99,7 +115,12 @@ const Resultpage = () => {
                       <h1>High Prices</h1>
                     </Select.Option>
                   </Select>
-                  <Select className="bg-gray-200 rounded-md" placeholder="Times" bordered={false} onSelect={lower}>
+                  <Select
+                    className="bg-gray-200 rounded-md"
+                    placeholder="Times"
+                    bordered={false}
+                    onSelect={lower}
+                  >
                     <Select.Option key={1} value="earliest-departure">
                       <h1>earliest departure</h1>
                     </Select.Option>
@@ -126,23 +147,42 @@ const Resultpage = () => {
               console.log(schedule);
               return (
                 <>
-                  <div className="sm:flex hidden justify-between items-center gap-4 bg-[#f1f5f5] rounded-md  px-5 py-8" key={i}>
+                  <div
+                    className="sm:flex hidden justify-between items-center gap-4 bg-[#f1f5f5] rounded-md  px-5 py-8"
+                    key={i}
+                  >
                     <div className="flex items-center">
                       <div className="mx-8">
                         <SiChinasouthernairlines size="2.5rem" />
                         <p className="text-sm">{schedule.airplaneName}</p>
                       </div>
                       <div>
-                        <p className="font-semibold text-xl">{schedule.departureTime}</p>
-                        <p className="font-light text-sm">{schedule.departureCity}</p>
+                        <p className="font-semibold text-xl">
+                          {schedule.departureTime}
+                        </p>
+                        <p className="font-light text-sm">
+                          {schedule.departureCity}
+                        </p>
                       </div>
-                      <AiOutlineArrowRight size="1.4rem" className="text-gray-400 mx-5" />
+                      <AiOutlineArrowRight
+                        size="1.4rem"
+                        className="text-gray-400 mx-5"
+                      />
                       <div>
-                        <p className="font-semibold text-xl">{schedule.arrivalTime}</p>
-                        <p className="font-light text-sm">{schedule.arrivalCity}</p>
+                        <p className="font-semibold text-xl">
+                          {schedule.arrivalTime}
+                        </p>
+                        <p className="font-light text-sm">
+                          {schedule.arrivalCity}
+                        </p>
                       </div>
                       <div className="duration mx-8  lg:block hidden">
-                        <p className="font-semibold text-xl">1H 30M</p>
+                        <p className="font-semibold text-xl">
+                          {convertDiff(
+                            schedule.departureTime,
+                            schedule.arrivalTime
+                          )}
+                        </p>
                         <p className="font-light text-sm">{schedule.status}</p>
                       </div>
                       <div className="items-center mx-8 lg:flex hidden">
@@ -159,29 +199,60 @@ const Resultpage = () => {
                       </div>
                     </div>
                     <div className="button-choose mr-12 md:block hidden">
-                      <button className="bg-blue-700 text-sm font-medium text-white rounded-md py-2 px-2 hover:bg-blue-600" onClick={() => navigate(`/flight/${schedule.scheduleId}?traveler=${traveler}`)}>
+                      <button
+                        className="bg-blue-700 text-sm font-medium text-white rounded-md py-2 px-2 hover:bg-blue-600"
+                        onClick={() =>
+                          navigate(
+                            `/flight/${schedule.scheduleId}?traveler=${traveler}`
+                          )
+                        }
+                      >
                         Choose Flight
                       </button>
                     </div>
                   </div>
 
                   {/* mobile phone */}
-                  <div className="justify-between items-center grid grid-cols-4 bg-[#f1f5f5] rounded-md my-2  px-5 py-8 sm:hidden" key={i} onClick={() => navigate(`/flight/${schedule.scheduleId}?traveler=${traveler}`)}>
+                  <div
+                    className="justify-between items-center grid grid-cols-4 bg-[#f1f5f5] rounded-md my-2  px-5 py-8 sm:hidden"
+                    key={i}
+                    onClick={() =>
+                      navigate(
+                        `/flight/${schedule.scheduleId}?traveler=${traveler}`
+                      )
+                    }
+                  >
                     <div className="mx-4">
                       <SiChinasouthernairlines size="2.5rem" />
                       <p className="text-sm">{schedule.airplaneName}</p>
                     </div>
                     <div>
-                      <p className="font-semibold text-xl">{schedule.departureTime}</p>
-                      <p className="font-light text-sm">{schedule.departureCity}</p>
+                      <p className="font-semibold text-xl">
+                        {schedule.departureTime}
+                      </p>
+                      <p className="font-light text-sm">
+                        {schedule.departureCity}
+                      </p>
                     </div>
-                    <AiOutlineArrowRight size="1.4rem" className="text-gray-400 mx-5" />
+                    <AiOutlineArrowRight
+                      size="1.4rem"
+                      className="text-gray-400 mx-5"
+                    />
                     <div>
-                      <p className="font-semibold text-xl">{schedule.arrivalTime}</p>
-                      <p className="font-light text-sm">{schedule.arrivalCity}</p>
+                      <p className="font-semibold text-xl">
+                        {schedule.arrivalTime}
+                      </p>
+                      <p className="font-light text-sm">
+                        {schedule.arrivalCity}
+                      </p>
                     </div>
                     <div className="duration mx-8  lg:block hidden">
-                      <p className="font-semibold text-xl">1H 30M</p>
+                      <p className="font-semibold text-xl">
+                        {convertDiff(
+                          schedule.departureTime,
+                          schedule.arrivalTime
+                        )}
+                      </p>
                       <p className="font-light text-sm">{schedule.status}</p>
                     </div>
                     <div className="items-center mx-8 lg:flex hidden">
@@ -198,7 +269,14 @@ const Resultpage = () => {
                     </div>
 
                     <div className="button-choose mr-12 md:block hidden">
-                      <button className="bg-blue-700 text-sm font-medium text-white rounded-md py-2 px-2 hover:bg-blue-600" onClick={() => navigate(`/flight/${schedule.scheduleId}?traveler=${traveler}`)}>
+                      <button
+                        className="bg-blue-700 text-sm font-medium text-white rounded-md py-2 px-2 hover:bg-blue-600"
+                        onClick={() =>
+                          navigate(
+                            `/flight/${schedule.scheduleId}?traveler=${traveler}`
+                          )
+                        }
+                      >
                         Choose Flight
                       </button>
                     </div>
